a0673e9b8ac9b88117dd3171140ffe1695ad643b
Crystal Chen <crystal.chen2@baruchmail.cuny.edu>
2024-12-02 01:37:23 +0000

    c

--- models/housing_dimensions/dim_location.sql
+++ models/housing_dimensions/dim_location.sql
@@ -1,6 +1,7 @@
 select row_number() over () as dim_location_id, *
 from
     (
-        select distinct borough, post_code as zip_code, council_district, block, problem_id
-                    FROM {{ref('cleaned_housing_maintenance_data')}})
-    
+        select distinct
+            borough, post_code as zip_code, council_district, block, problem_id
+        from {{ ref("cleaned_housing_maintenance_data") }}
+    )
