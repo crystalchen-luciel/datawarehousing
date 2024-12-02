a1d15cd5a8c9675dd9076dbc83ca1cdc645bba12
Crystal Chen <crystal.chen2@baruchmail.cuny.edu>
2024-12-02 00:42:31 +0000

    c

--- models/staging/cleaned_311_data_test_B.sql
+++ models/staging/cleaned_311_data_test_B.sql
@@ -1,12 +1,15 @@
-select unique_key, 
-    format_datetime('%Y-%m-%d',PARSE_DATETIME('%m/%d/%Y %I:%M:%S %p', created_date)) as created_date, 
-    upper(borough) as borough, 
-    upper(city) as city, 
+select
+    unique_key,
+    format_datetime(
+        '%Y-%m-%d', parse_datetime('%m/%d/%Y %I:%M:%S %p', created_date)
+    ) as created_date,
+    upper(borough) as borough,
+    upper(city) as city,
     upper(complaint_type) as complaint_type,
     incident_zip
-from {{ ref('nyc_311_data') }}
+from {{ ref("nyc_311_data") }}
 where
-        unique_key is not null
-        and borough is not null
-        and city is not null
-        and incident_zip is not null
+    unique_key is not null
+    and borough is not null
+    and city is not null
+    and incident_zip is not null
