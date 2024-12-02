9baacccd414c4ebfa3d66073a0da94862f1eb8cf
Crystal Chen <crystal.chen2@baruchmail.cuny.edu>
2024-12-02 00:14:34 +0000

    c

--- models/conformed_dimension/dim_location_joined_test_B.sql
+++ models/conformed_dimension/dim_location_joined_test_B.sql
@@ -1,18 +1,23 @@
 with
-    location_311 as (select * from {{ ref('dim_location_311') }}),
-    location_housing as (select * from {{ ref('dim_location') }}),
+    location_311 as (select * from {{ ref("dim_location_311") }}),
+    location_housing as (select * from {{ ref("dim_location") }}),
     dim_location_joined as (
-        select
-            row_number() over() as dim_location_joined_id, *
-            from
-            (select distinct
-            coalesce(loc311.borough, loc_housing.borough) as borough,
-            coalesce(loc311.zip_code, loc_housing.zip_code) as zip_code,
-            city, loc_housing.council_district, loc_housing.block
-        from location_311 loc311
-        full outer join location_housing loc_housing
-            on loc311.borough = loc_housing.borough
-            and loc311.zip_code = loc_housing.zip_code 
-            ))
+        select row_number() over () as dim_location_joined_id, *
+        from
+            (
+                select distinct
+                    coalesce(loc311.borough, loc_housing.borough) as borough,
+                    coalesce(loc311.zip_code, loc_housing.zip_code) as zip_code,
+                    city,
+                    loc_housing.council_district,
+                    loc_housing.block
+                from location_311 loc311
+                full outer join
+                    location_housing loc_housing
+                    on loc311.borough = loc_housing.borough
+                    and loc311.zip_code = loc_housing.zip_code
+            )
+    )
 
-    select  * from dim_location_joined
+select *
+from dim_location_joined
